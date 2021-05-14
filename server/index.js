cdaec3c8d78a328b1cadbc8216830baa8b62d23d
Anderson Tonon <awst@live.com>
2021-05-14 15:38:13 -0300

    Mudança no servidor node para uso de Express

--- server/index.js
+++ server/index.js
@@ -1,8 +1,15 @@
-  
 const http = require('http');
-const fs = require('fs');
+const path = require('path');
+//const fs = require('fs');
+const express = require('express')
+const server = express();
 const url = require('url');
 const qs = require('querystring');
+const Fipe = require('../model/Fipe.js');
+
+server.set('view engine', 'hbs');
+server.set('views',path.join(__dirname, '../view'));
+server.use(express.static(path.join(__dirname, '../public')));
 
 const parseCookies = (cookie = '') =>
     cookie
@@ -16,12 +23,93 @@ const parseCookies = (cookie = '') =>
 
 const session = {};
 
-http.createServer((req, res) => {
+server.get('/' || '',(req, res) =>{
+    res.render('busca');
+
+});
+server.get('/fipe/tipo', async (req, res) => {
+    
+    const tipo = req.query.tipo;
+    const resBusca = await Fipe.find('TIPO', tipo);
+    var dados = [];
+
+    for(let value of resBusca){
+        dados.push({value});
+    }
+    res.send({dados});
+});
+
+server.get('/fipe/tipo/marca', async (req, res) => {
+
+    const tipo = req.query.tipo;
+    const marca = req.query.marca;
+    const resBusca = await Fipe.find('MARCA' ,tipo, marca);
+
+    var dados = [];
+
+    for(let value of resBusca){
+        dados.push({value});
+    }
+    res.send({dados});
+});
+
+server.get('/fipe/tipo/marca/modelo', async (req, res) => {
+
+    const tipo = req.query.tipo;
+    const marca = req.query.marca;
+    const modelo = req.query.modelo;
+    const resBusca = await Fipe.find('MODELO' ,tipo, marca, modelo);
+
+    var dados = [];
+    for(let value of resBusca){
+        dados.push({value});
+    }
+    res.send({dados});
+});
+
+server.get('/fipe/tipo/marca/modelo/ano', async (req, res) => {
+
+    const tipo = req.query.tipo;
+    const marca = req.query.marca;
+    const modelo = req.query.modelo;
+    const ano = req.query.ano;
+    const resBusca = await Fipe.find('ANO' ,tipo, marca, modelo, ano);
+    
+    res.send(resBusca[0]);
+});
+
+server.get('/login', (req, res) => {
+    res.render('paginaLogin', (err,data) => {
+        if (err) {
+            throw err;
+        }
+        res.end(data);
+    });
+});
+
+server.get('/busca', (req, res) => {
+    res.render('busca', (err,data) => {
+        if (err) {
+            throw err;
+        }
+        res.end(data);
+    });
+});
+
+
+server.get('/entrar', (req, res) => {
+
     const cookies = parseCookies(req.headers.cookie);
     
-    if(req.url.startsWith('/login')) {
-        const { query } = url.parse(req.url);
-        const { email } = qs.parse(query);
+    console.log(cookies.session);
+
+    if (session[cookies.session] && session[cookies.session].email =='awst@live.com' && session[cookies.session].expires > new Date()) {
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
+        res.end(`Bem vindo ${session[cookies.session].email}`);
+
+    }else if(req.url.startsWith('/entrar')){ 
+        const email = req.query.email;
+        console.log(email);
         const expires = new Date();
         expires.setMinutes(expires.getMinutes() + 1);
         const randomInt = +new Date();
@@ -29,24 +117,22 @@ http.createServer((req, res) => {
             email,
             expires
         };
+        //res.setHeader
         res.writeHead(302, {
-            Location: '/',
-            'Set-Cookie': `session=${randomInt};Expires=${expires.toUTCString()};HttpOnly;Path=/`,
+            Location: '/entrar', 'Set-Cookie': `session=${randomInt};Expires=${expires.toUTCString()};HttpOnly;Path=/`,
         });
         res.end();
-    } else if (cookies.session && session[cookies.session].email =='awst@live.com' && session[cookies.session].expires > new Date()) {
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8'});
-        res.end(`Bem vindo ${session[cookies.session].email}`)
-    } else {
-        fs.readFile('client/login.html', (err,data) => {
+     }else{
+        res.render('paginaLogin', (err,data) => {
             if (err) {
                 throw err;
             }
             res.end(data);
-        });
-    }
-})
-    .listen(8090, () => {
+        });        
+    }  
+});
 
-        console.log('listening on port 8090');
-    })
+server.listen(8090, "192.168.0.120", () =>{
+    console.log('Servidor iniciado em http://localhost:8090');
+    console.log('Para desligar o servidor: ctrl + c');
+});
